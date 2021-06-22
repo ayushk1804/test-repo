c96c95c0cb09f5486840fb1d13b468ffe4e68df9
ayushk1804 <ayushk780@gmail.com>
2021-06-22 18:31:46 +0530

    fixes #1 Changed header

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Hello World</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Hello{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
